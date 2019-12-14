a741f69b60080636dc148558dcdafb78e42a229c
akshay <akshay@akshay-HP-Pavilion-g6-Notebook-PC>
2019-12-14 15:36:13 +0900

    new program

---
+++ OnlineCoding/src/online/coding/basic/GoogleMirrorChange.java
@@ -0,0 +1,12 @@
+package online.coding.basic;
+
+import java.util.Scanner;
+
+public class GoogleMirrorChange {
+
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+  System.out.println("1");
+	}
+
+}
