bd4aee86d8871506ef1930dbddfd038e9cbedcf5
akshay <akshay@akshay-HP-Pavilion-g6-Notebook-PC>
2019-12-15 16:24:53 +0900

    changes

--- OnlineCoding/src/online/coding/basic/GoogleMirrorChange.java
+++ OnlineCoding/src/online/coding/basic/GoogleMirrorChange.java
@@ -6,7 +6,29 @@ public class GoogleMirrorChange {
 
 	public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-  System.out.println("1");
+    int t=sc.nextInt();
+    while((t--)>0) {
+    	 int n=sc.nextInt();
+         int[][] a=new int[n][n];
+    	 for(int i=0;i<n;i++) {
+    	    for(int j=0;j<n;j++) {
+    	      a[i][j]=sc.nextInt(); 
+    	     }
+    	  }
+    	for(int i=0;i<n;i++) {
+     	    for(int j=0;j<n;j++) {
+     	      System.out.print(a[i][j]); 
+     	     }
+  	      System.out.println();  
+    	}
+    	for(int i=0;i<n;i++) {
+     	    for(int j=0;j<n;j++) {
+     	  	  System.out.print(a[i][j]);
+     	    }
+    	  System.out.println();
+    	  }
+    	
+        }
+    System.out.println("1");
 	}
-
 }
